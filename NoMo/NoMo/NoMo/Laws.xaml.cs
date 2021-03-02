fcbd92b26ed16bd92cfb55dfb46cdda6c379f54e
Mikaela Gabriels <mikaelagabriels@gmail.com>
2021-03-02 14:31:47 +0200

    Laws, Protocol and Motivational Quotes Pages Created

---
+++ NoMo/NoMo/NoMo/Laws.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace NoMo
+{
+   
+    public partial class Laws : ContentPage
+    {
+        public Laws()
+        {
+            InitializeComponent();
+
+            Title = "Legal Education Laws";
+        }
+    }
+}
---
+++ NoMo/NoMo/NoMo/MotivationQuotes.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace NoMo
+{
+    public partial class MotivationQuotes : ContentPage
+    {
+        public MotivationQuotes()
+        {
+            InitializeComponent();
+
+            Title = "Motivational Quotes";
+        }
+    }
+}
