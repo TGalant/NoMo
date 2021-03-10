c1383c33a10a18b5bd1b281d40cd0b0dcbd5e12a
Mikaela Gabriels <mikaelagabriels@gmail.com>
2021-03-10 14:01:01 +0200

    Added Pledge Page

---
+++ NoMo/NoMo/NoMo/Testimonials.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NoMo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Testimonials : ContentPage
+    {
+        public Testimonials()
+        {
+            InitializeComponent();
+
+            Title = "Testimonials";
+        }
+    }
+}
