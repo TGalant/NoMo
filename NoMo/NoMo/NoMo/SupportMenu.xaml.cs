be990fd914e31295827b84a779b1b819c7d6a3e9
Mikaela Gabriels <mikaelagabriels@gmail.com>
2021-03-17 15:30:48 +0200

    Added Remaining Regions

--- NoMo/NoMo/NoMo/SupportMenu.xaml.cs
+++ NoMo/NoMo/NoMo/SupportMenu.xaml.cs
@@ -15,5 +15,30 @@ namespace NoMo
         {
             Navigation.PushAsync(new CapePeninsulaRegion());
         }
+
+        private void TapGestureRecognizer_Tapped_1(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new CapeWinelandsRegion());
+        }
+
+        private void TapGestureRecognizer_Tapped_2(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new CentralKarooRegion());
+        }
+
+        private void TapGestureRecognizer_Tapped_3(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new GardenRouteAndKleinKarooRegion());
+        }
+
+        private void TapGestureRecognizer_Tapped_4(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new OverbergRegion());
+        }
+
+        private void TapGestureRecognizer_Tapped_5(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new WestCoastRegion());
+        }
     }
 }
